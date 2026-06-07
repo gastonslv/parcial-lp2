ef788dc8eb5d18792779b464134f3c7566ddd7b6
Gastón Silvestre <gastonsilvestrebrc@gmail.com>
2026-06-07 19:18:29 -0300

    corrección de conexión

--- config/conexion.php
+++ config/conexion.php
@@ -9,9 +9,4 @@ define('DB_NAME', 'concesionario_db');
 
 // Crear la conexión
 $conexion = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-// Verificar si hubo error al conectar
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
 ?>
